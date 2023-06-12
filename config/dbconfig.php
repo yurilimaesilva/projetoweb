5a9f7d70762c58f1640712fb5ae14a48aeeae3c2
yurilimaesilva <yurilimaesilva1@gmail.com>
2023-06-12 12:33:06 -0300

    pasta Config

---
+++ config/dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+
+define('HOST','localhost');
+define('USER','root');
+define('PASSWORD','');
+define('DBNAME','bd_test');
+define('IMG','http://localhost/Junior/Library/Images');
+
